92780e1b0f62590743aac2ae21fc019e0cd0be5e
tagadanar <raphael.renon@gmail.com>
2019-05-07 18:25:03 +0200

    Create monkey.js

---
+++ monkey.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         Auto Close Sponsored fb
+// @namespace    test
+// @version      1.0.0
+// @description  Auto Close Sponsored fb
+// @author       taga
+// @match        *://*.facebook.com/*
+// @require https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js 
+// ==/UserScript==
+
+function hideSponsored({target}){
+  var regexHyperfeed = /hyperfeed_story_id_.*/;
+  if(target.id.match(regexHyperfeed)){
+    //console.log(target.textContent);
+    //console.log(target.innerText);
+    var regexSponso = /.*Sponsorisé.*/;
+    if(target.innerText.match(regexSponso)){
+      setTimeout(function(){ target.remove(); console.log("ad blocked: " + target.id); }, 10);
+    }
+  }
+}
+
+function handleMutation(mutation){
+	mutation.forEach(hideSponsored);
+}
+
+function addWatcher(){
+	const feed = document.getElementById('stream_pagelet');
+	const config = { attributes: false, childList: true, subtree: true };
+	const observer = new MutationObserver(handleMutation);
+	observer.observe(feed, config);
+}
+
+
+addWatcher();
